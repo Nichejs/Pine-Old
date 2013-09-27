859dad9dc32f4ed5697544909b6c364c1be5a4a9
aurbano <alejandro@urbanoalvarez.es>
2013-09-27 18:51:28 +0200

    Move things to public folder

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-/*var http = require('http');
-
-var index = fs.readFileSync('index.html');
-
-http.createServer(function(req,resp) {
-	resp.writeHead(200, {"Content-Type": "text/html"});
-	
-	resp.write(index);
-	resp.end();
-	
-	console.log("sample output to console");
-
-}).listen(8080);*/
-var connect = require('connect');
+/*var connect = require('connect');
 connect.createServer(
     connect.static(__dirname)
-).listen(8080);
+).listen(8080);*/
+var connect = require('connect')
+  , http = require('http');
+
+var app = connect()
+  .use(connect.favicon())
+  .use(connect.logger('dev'))
+  .use(connect.static('public'))
+  .use(connect.directory('public'))
+  .use(connect.cookieParser())
+  .use(connect.session({ secret: 'no se que es esto' }))
+  .use(function(req, res){
+    res.end('Servidor de Open RPG\n');
+  });
+
+http.createServer(app).listen(8080);
