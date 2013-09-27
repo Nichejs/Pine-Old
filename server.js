fcbac0530373f3b60f34901b99aeae1380be2182
aurbano <alejandro@urbanoalvarez.es>
2013-09-27 19:23:10 +0200

    Serve files from public folder

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
-/*var connect = require('connect');
+// Sirve los contenidos de la carpeta "public"
+var connect = require('connect');
 connect.createServer(
-    connect.static(__dirname)
-).listen(8080);*/
-var connect = require('connect')
-  , http = require('http');
-
-var app = connect()
-  .use(connect.favicon())
-  .use(connect.logger('dev'))
-  .use(connect.static('public'))
-  .use(connect.directory('public'))
-  .use(connect.cookieParser())
-  .use(connect.session({ secret: 'no se que es esto' }))
-  .use(function(req, res){
-    res.end('Servidor de Open RPG\n');
-  });
-
-http.createServer(app).listen(8080);
+    connect.static(__dirname + '/public')
+).listen(8080);
