1b40966fe9cf44cc02d0964e5cde9394fb7a6367
aurbano <alejandro@urbanoalvarez.es>
2013-10-14 11:07:13 +0200

    Register

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var express = require('express'),
 	http = require('http'),
 	server = http.createServer(app),
 	path = require('path'),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	nano = require('nano')('http://pi:pi@localhost:8000'),
+	crypto = require('crypto'),
+	sha1 = crypto.createHash('sha1');
 
 
 server.listen(3000);
@@ -12,16 +15,15 @@ console.log("Express server listening on port 3000");
 
 // Allow access to /public folder
 app.configure(function () {
+	app.use(express.bodyParser());
 	app.use(app.router);
     app.use(express.static(path.join(__dirname,'/public'), {maxAge: 0}));
 });
 
 // CouchDB Access
 app.get('/api/db', function (req, res) {
-	// Nano!
-    var nano = require('nano')('http://pi:pi@localhost:8000');
-    
-    // Users handle
+	
+	// Users handle
     var users = nano.use('users');
     var response = '';
     users.view('userList', 'userList', function(err, body) {
@@ -37,6 +39,28 @@ app.get('/api/db', function (req, res) {
 	});
 });
 
+// Requests
+app.post('/api/post', function(request, response){
+    
+    var data = request.body.user;
+    
+    console.log("Received data: ", data);
+    
+    var users = nano.use('users');
+    // Sha1 of password
+    sha1.update(data.pass);
+    // Insert in database
+    //users.insert(data.name, {name: data.name, pass: sha1.digest('hex')});
+    console.log("Intentando insertar");
+    users.insert({name: data.name, pass: sha1.digest('hex')}, function(err, body) {
+		if (err) console.err(err);
+		console.log('done');
+	});
+    
+    response.send("Recibido!");
+    response.end();
+
+});
 
 // SocketIO
 io.sockets.on('connection', function (socket) {
